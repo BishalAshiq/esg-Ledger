26f7f85866e93b5474387faabadc11e7931e4b30
Md. Ashiqul Islam <ashiqul.ba@pakiza.com>
2023-11-18 01:45:49 +0600

    login and forget password done

---
+++ src/component/DashboardAll/ViewAllItems.jsx
@@ -0,0 +1,163 @@
+import React from "react";
+
+const ViewAllItems = () => {
+  return (
+    <div>
+      <div className='ViewAll-Items-div'>
+        <div className='row checkbox-div'>
+          <div className='col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 '>
+            <div className='check-div'>
+              <div class='form-check'>
+                <input
+                  class='form-check-input'
+                  type='checkbox'
+                  value=''
+                  id='flexCheckChecked'
+                  checked
+                />
+                <label class='form-check-label' for='flexCheckChecked'>
+                  Checked checkbox
+                </label>
+              </div>
+
+              <span>View all items - customise data field</span>
+            </div>
+          </div>
+
+          <div className='col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 '>
+            <div className='view-addbtn-div'>
+              <span>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='16'
+                  height='16'
+                  fill='currentColor'
+                  class='bi bi-exclamation-circle-fill'
+                  viewBox='0 0 16 16'>
+                  <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z' />
+                </svg>
+              </span>
+              <button className='view-add-btn'>+ Add more</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='row checkbox-div'>
+          <div className='col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 '>
+            <div className='check-div'>
+              <div class='form-check'>
+                <input
+                  class='form-check-input'
+                  type='checkbox'
+                  value=''
+                  id='flexCheckChecked'
+                  checked
+                />
+                <label class='form-check-label' for='flexCheckChecked'>
+                  Checked checkbox
+                </label>
+              </div>
+
+              <span>View all items - customise data field</span>
+            </div>
+          </div>
+
+          <div className='col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 '>
+            <div className='view-addbtn-div'>
+              <span>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='16'
+                  height='16'
+                  fill='currentColor'
+                  class='bi bi-exclamation-circle-fill'
+                  viewBox='0 0 16 16'>
+                  <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z' />
+                </svg>
+              </span>
+              <button className='view-add-btn'>+ Add more</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='row checkbox-div'>
+          <div className='col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 '>
+            <div className='check-div'>
+              <div class='form-check'>
+                <input
+                  class='form-check-input'
+                  type='checkbox'
+                  value=''
+                  id='flexCheckChecked'
+                  checked
+                />
+                <label class='form-check-label' for='flexCheckChecked'>
+                  Checked checkbox
+                </label>
+              </div>
+
+              <span>View all items - customise data field</span>
+            </div>
+          </div>
+
+          <div className='col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 '>
+            <div className='view-addbtn-div'>
+              <span>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='16'
+                  height='16'
+                  fill='currentColor'
+                  class='bi bi-exclamation-circle-fill'
+                  viewBox='0 0 16 16'>
+                  <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z' />
+                </svg>
+              </span>
+              <button className='view-add-btn'>+ Add more</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='row checkbox-div'>
+          <div className='col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 '>
+            <div className='check-div'>
+              <div class='form-check'>
+                <input
+                  class='form-check-input'
+                  type='checkbox'
+                  value=''
+                  id='flexCheckChecked'
+                  checked
+                />
+                <label class='form-check-label' for='flexCheckChecked'>
+                  Checked checkbox
+                </label>
+              </div>
+
+              <span>View all items - customise data field</span>
+            </div>
+          </div>
+
+          <div className='col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 '>
+            <div className='view-addbtn-div'>
+              <span>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='16'
+                  height='16'
+                  fill='currentColor'
+                  class='bi bi-exclamation-circle-fill'
+                  viewBox='0 0 16 16'>
+                  <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z' />
+                </svg>
+              </span>
+              <button className='view-add-btn'>+ Add more</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ViewAllItems;
