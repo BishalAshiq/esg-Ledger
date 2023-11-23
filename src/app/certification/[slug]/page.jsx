533fa652276b29833559cae3fb3e47f98f8b356d
Md Anik Islam <anikislam668@gmail.com>
2023-11-23 17:48:35 +0600

    changrd

--- src/app/certification/[slug]/page.jsx
+++ src/app/certification/[slug]/page.jsx
@@ -53,90 +53,93 @@ const page = () => {
           <div className='row mt-4'>
             <div className='col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2'></div>
 
-            <div className='col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 redix-div'>
-              <div className='redix-divs'>
-                <div className='certificate-image-div'>
-                  <Image
-                    className='certificate-image'
-                    src={certified.src}
-                    width={44}
-                    height={40}
-                    alt=''
-                  />
-                </div>
+            <div className='col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3'>
+              <div className=" redix-div">
+                <div className='redix-divs'>
+                  <div className='certificate-image-div'>
+                    <Image
+                      className='certificate-image'
+                      src={certified.src}
+                      width={44}
+                      height={40}
+                      alt=''
+                    />
+                  </div>
 
-                <div className='certificate-tags-div'>
-                  <h2 className='certificate-tags'>
-                    THE BLOCKCHAIN CERTIFICATE
-                  </h2>
-                  <p className='certificate-ptag'>區塊鏈防偽證書</p>
-                  <p className='certificate-ptag'>by OneChain®</p>
-                </div>
+                  <div className='certificate-tags-div'>
+                    <h2 className='certificate-tags'>
+                      THE BLOCKCHAIN CERTIFICATE
+                    </h2>
+                    <p className='certificate-ptag'>區塊鏈防偽證書</p>
+                    <p className='certificate-ptag'>by OneChain®</p>
+                  </div>
 
-                <div className='certificate-des-div'>
-                  <h5 className='certificate-h5Tag'>
-                    This Certificate has been securely recorded on the
-                    blockchain, ensuring its transparency and immutable record.
-                    It is applicable to products produced in strict compliance
-                    with ESG requirements as listed below.
-                  </h5>
+                  <div className='certificate-des-div'>
+                    <h5 className='certificate-h5Tag'>
+                      This Certificate has been securely recorded on the
+                      blockchain, ensuring its transparency and immutable record.
+                      It is applicable to products produced in strict compliance
+                      with ESG requirements as listed below.
+                    </h5>
 
-                  <h5 className='certificate-h5Tag'>
-                    此證書已安全記錄於區塊鏈上，確保其透明性和不可變紀錄。適用於嚴格符合以下列出的ESG要求的產品
-                  </h5>
-                </div>
+                    <h5 className='certificate-h5Tag'>
+                      此證書已安全記錄於區塊鏈上，確保其透明性和不可變紀錄。適用於嚴格符合以下列出的ESG要求的產品
+                    </h5>
+                  </div>
 
-                <div className='certificate-imageSec-div'>
-                  <img src={redix.src} alt='' />
+                  <div className='certificate-imageSec-div'>
+                    <img src={redix.src} alt='' />
+                  </div>
                 </div>
               </div>
+
             </div>
 
             <div className='col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 '>
 
-            <div className='redix-sec-div'>
-              <div className='redixSec-div'>
-                <div className='Blockchain-tagone-div'>
-                  <h5 className='Blockchain-tagone'>
-                    Blockchain Transaction Hx 區塊鏈上鏈紀錄
-                  </h5>
-                </div>
-
-                <div className='Blockchain-tagotwo-div'>
-                  <Image src={copy.src} width={20} height={20} alt='' />
-                  <h5 className='Blockchain-tagotwo'>
-                    {item.block_chain_url}
-                  </h5>
-                </div>
-
-                <div className='Blockchain-tagone-div'>
-                  <h5 className='Blockchain-tagone'>
-                    Issue Date 區塊鏈上鏈日期
-                  </h5>
-                </div>
-
-                <div className='Blockchain-ptag-div'>
-                  <p>{formateDate(item.created_at)}</p>
-                </div>
-                <div className='Blockchain-ptag-divs'>
-                  <p className='block-ptext'>Brand Name 型號</p>
-                  <h5 className='blockchain-h5'> {item.brand_name}</h5>
-                </div>
-                <div className='Blockchain-ptag-divs'>
-                  <p className='block-ptext'>Item 物品</p>
-                  <h5 className='blockchain-h5'> {item.item}</h5>
-                  <h5 className='blockchain-h5'>木製復古遊戲機 </h5>
-                </div>
-                {Object.entries(attribute).map(([key, value]) => (
-                  <div className='Blockchain-ptag-divs'>
-                    <p className='block-ptext'>{key} 來源</p>
-                    <h5 className='blockchain-h5'>
-                      {value} <br /> 香港本土
-                      土木工程(斜坡維修)及颱風倒塌樹木進行升級改造{" "}
+              <div className='redix-sec-div'>
+                <div className='redixSec-div'>
+                  <div className='Blockchain-tagone-div'>
+                    <h5 className='Blockchain-tagone'>
+                      Blockchain Transaction Hx 區塊鏈上鏈紀錄
                     </h5>
                   </div>
-                ))}
-                {/* <div className='Blockchain-ptag-divs'>
+
+                  <div className='Blockchain-tagotwo-div'>
+                    <Image src={copy.src} width={20} height={20} alt='' />
+                    <h5 className='Blockchain-tagotwo'>
+                      {item.block_chain_url}
+                    </h5>
+                  </div>
+
+                  <div className='Blockchain-tagone-div'>
+                    <h5 className='Blockchain-tagone'>
+                      Issue Date 區塊鏈上鏈日期
+                    </h5>
+                  </div>
+
+                  <div className='Blockchain-ptag-div'>
+                    <p>{formateDate(item.created_at)}</p>
+                  </div>
+                  <div className='Blockchain-ptag-divs'>
+                    <p className='block-ptext'>Brand Name 型號</p>
+                    <h5 className='blockchain-h5'> {item.brand_name}</h5>
+                  </div>
+                  <div className='Blockchain-ptag-divs'>
+                    <p className='block-ptext'>Item 物品</p>
+                    <h5 className='blockchain-h5'> {item.item}</h5>
+                    <h5 className='blockchain-h5'>木製復古遊戲機 </h5>
+                  </div>
+                  {Object.entries(attribute).map(([key, value]) => (
+                    <div className='Blockchain-ptag-divs'>
+                      <p className='block-ptext'>{key} 來源</p>
+                      <h5 className='blockchain-h5'>
+                        {value} <br /> 香港本土
+                        土木工程(斜坡維修)及颱風倒塌樹木進行升級改造{" "}
+                      </h5>
+                    </div>
+                  ))}
+                  {/* <div className='Blockchain-ptag-divs'>
                   <p className='block-ptext'>Product - Front 產品正面</p>
                   <h5 className='blockchain-h5'>
                     Hong Kong Timber - Oak 香港木材 - 橡木
@@ -179,31 +182,31 @@ const page = () => {
                   </h5>
                 </div> */}
 
-                <div className='Information-full-div'>
-                  <p>Important Information:</p>
-                  <ul>
-                    <ol>
-                      1. This certificate is non-transferable and is permanently
-                      recorded on the blockchain
-                    </ol>
-                    <ol>
-                      2. For any inquiries or conernsm, feel free to contact us
-                      at enquiries@certledger.io
-                    </ol>
-                  </ul>
+                  <div className='Information-full-div'>
+                    <p>Important Information:</p>
+                    <ul>
+                      <ol>
+                        1. This certificate is non-transferable and is permanently
+                        recorded on the blockchain
+                      </ol>
+                      <ol>
+                        2. For any inquiries or conernsm, feel free to contact us
+                        at enquiries@certledger.io
+                      </ol>
+                    </ul>
+                  </div>
+                  <div className='Information-full-div'>
+                    <p>重要信息::</p>
+                    <ul>
+                      <ol>1. 此證書不可轉讓，已永久記錄在區塊鏈上。</ol>
+                      <ol>
+                        2. 如有任何疑問或關切事
+                        className="block-ptext"項，可電郵至:
+                        enquiries@certledger.ioh5 className="blockchain-h5"
+                      </ol>
+                    </ul>
+                  </div>
                 </div>
-                <div className='Information-full-div'>
-                  <p>重要信息::</p>
-                  <ul>
-                    <ol>1. 此證書不可轉讓，已永久記錄在區塊鏈上。</ol>
-                    <ol>
-                      2. 如有任何疑問或關切事
-                      className="block-ptext"項，可電郵至:
-                      enquiries@certledger.ioh5 className="blockchain-h5"
-                    </ol>
-                  </ul>
-                </div>
-              </div>
               </div>
             </div>
             <div className='col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2'></div>
