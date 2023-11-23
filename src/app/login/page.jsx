34db71853cf5918d4e81b90e79b626b490707418
Md. Ashiqul Islam <ashiqul.ba@pakiza.com>
2023-11-23 15:43:20 +0600

    updated

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -137,11 +137,50 @@ const page = () => {
         </div>
       </div>
 
-      <div className='login-footer-div'>
-        <div className='login-footer-full-div row'>
+      <div className='login-footer-banner-full-div'>
+        <div className='row'>
           <div className='col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2'></div>
+          <div className='col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8'>
+            <div className='login-footer-banner-div'>
+              <div className=''>
+                <img className='footer-img' src={pageLogo.src} alt='' />
+                <p className='fotter-ptext'>
+                  For inquiries or more information please contact:
+                  enquiries@esgledger.co
+                </p>
+              </div>
 
-          <div className='col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4'>
+              <div className=''>
+                <p className='footer-textpp'>Join the Community</p>
+
+                <div className='footer-text-icon'>
+                  <img className='social-icon' src={pageicon1.src} alt='' />
+                  <img className='social-icon' src={pageicon2.src} alt='' />
+                  <img className='social-icon' src={pageicon3.src} alt='' />
+                  <img className='social-icon' src={pageicon4.src} alt='' />
+                </div>
+              </div>
+            </div>
+            <div className=''>
+              <hr className='f-hr' />
+              <p className='f-text'>© 2023 OneChain Ltd.</p>
+            </div>
+          </div>
+          <div className='col-2 col-sm-2 col-md-2 col-lg-2 col-xl-3'></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
+
+{
+  /* <div className='login-footer-div'>
+        <div className='login-footer-full-div row'>
+          <div className='col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3'></div>
+
+          <div className='col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6'>
             <div className='footer-img-div'>
               <img className='footer-img' src={pageLogo.src} alt='' />
               <p className='fotter-ptext'>
@@ -149,9 +188,6 @@ const page = () => {
                 enquiries@esgledger.co
               </p>
             </div>
-          </div>
-
-          <div className='col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4'>
             <div className='footer-text-div'>
               <p className='footer-textpp'>Join the Community</p>
 
@@ -162,12 +198,19 @@ const page = () => {
                 <img className='social-icon' src={pageicon4.src} alt='' />
               </div>
             </div>
+
+            <div className=''>
+              <hr className='f-hr' />
+              <p className='f-text'>© 2023 OneChain Ltd.</p>
+            </div>
           </div>
 
-          <div className='col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2'></div>
+       
+
+          <div className='col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6'></div>
         </div>
 
-        <div className='ffs-div'>
+         <div className='ffs-div'>
           <div className='row'>
             <div className='col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2'></div>
             <div className='col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8'>
@@ -178,10 +221,12 @@ const page = () => {
             </div>
             <div className='col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2'></div>
           </div>
-        </div>
-      </div>
+        </div> 
+      </div> */
+}
 
-      {/* <div>
+{
+  /* <div>
         <div className='hr' />
         <div className=' login-banner-img-input-div'>
           <div className='login-banner-img-div'>
@@ -213,9 +258,5 @@ const page = () => {
             <br /> platform under OneChain®
           </p>
         </div>
-      </div> */}
-    </div>
-  );
-};
-
-export default page;
+      </div> */
+}
