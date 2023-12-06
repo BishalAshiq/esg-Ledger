2bf9452203756505094098daeb461fac5dbd5207
Md Anik Islam <anikislam668@gmail.com>
2023-12-06 17:13:59 +0600

    changes on folder structure

---
+++ src/app/admin/brands/create/page.jsx
@@ -0,0 +1,70 @@
+"use client";
+import React, { useState } from "react";
+import DashNav from "@/component/Navbar/dashNav";
+import ViewAllItems from "@/component/DashboardAll/ViewAllItems";
+import IssuDetails from "@/component/DashboardAll/IssuDetails";
+import ViewAllItemsData from "@/component/DashboardAll/ViewAllItemsData";
+import IndividualBrand from "@/component/AllAdminBrand/IndividualBrand";
+
+const page = () => {
+  const [activeDiv, setActiveDiv] = useState(1);
+  const handleTextClick = (divId) => {
+    setActiveDiv(divId);
+  };
+  return (
+    <div>
+      <div>
+        <DashNav />
+      </div>
+
+      <div className='row'>
+        <div className='col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2'>
+          <div className='dddssa' />
+          <div className='dash-sidebar-div'>
+            <div
+              onClick={() => handleTextClick(1)}
+              className={`dashboard-side-bar-li ${
+                activeDiv === 1 ? "active-das" : ""
+              }`}>
+              <h6 className='dash-side-ptext'>View all brands</h6>
+            </div>
+            <div
+              onClick={() => handleTextClick(2)}
+              className={`dashboard-side-bar-li ${
+                activeDiv === 2 ? "active-das" : ""
+              }`}>
+              <h6 className='dash-side-ptext'>View all items</h6>
+            </div>
+          </div>
+        </div>
+
+        <div className='col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10'>
+          {activeDiv === 1 && (
+            <div>
+              <div className=''>
+                <IndividualBrand />
+              </div>
+            </div>
+          )}
+
+          {activeDiv === 2 && (
+            <div>
+              <div className=''>
+                <ViewAllItemsData />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className='dashboard-footer-div'>
+        <div className='dashboard-footer'>
+          <p className='dashboard-footer-text'>
+            © 2023 OneChain Ltd. All Rights Reserved.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
