43c97c0c032beec346832c7e9d1b98f9dd3433d6
Md. Ashiqul Islam <ashiqul.ba@pakiza.com>
2023-12-12 17:48:27 +0600

    bugs solve

--- src/component/Navbar/Nav.jsx
+++ src/component/Navbar/Nav.jsx
@@ -105,7 +105,7 @@ const Nav = () => {
                 aria-label='Toggle navigation'>
                 <span className=''>
                   {" "}
-                  <svg
+                  {/* <svg
                     xmlns='http://www.w3.org/2000/svg'
                     width='26'
                     height='26'
@@ -116,7 +116,7 @@ const Nav = () => {
                       fill-rule='evenodd'
                       d='M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5'
                     />
-                  </svg>
+                  </svg> */}
                 </span>
               </button>
             </div>
