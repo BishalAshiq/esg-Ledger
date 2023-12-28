4bb843e3a0122eb17f5bf4669cc57e1adf4044ca
morshedulmunna <morshedulmunna1@gmail.com>
2023-12-28 11:51:55 +0600

    fix: navigation icons

--- src/component/DashboardAll/ViewAllItemsData.jsx
+++ src/component/DashboardAll/ViewAllItemsData.jsx
@@ -1,12 +1,12 @@
 "use client";
-import React, { useEffect, useState, useRef } from "react";
+import React, {useEffect, useState, useRef} from "react";
 import Downloadicon from "../../../public/downloadicon.svg";
 import QrCode from "../../../public/qrcode.svg";
 import Image from "next/image";
 import axiosInstance from "../../../utils/axios";
 import QRCodeComponent from "./QRCodeComponent";
-import { useRouter } from "next/navigation";
-import { toast } from "react-toastify";
+import {useRouter} from "next/navigation";
+import {toast} from "react-toastify";
 
 const ViewAllItemsData = () => {
   const itemsPerPage = 15;
@@ -22,28 +22,30 @@ const ViewAllItemsData = () => {
       token = localStorage.getItem("refreshToken");
     }
 
-    axiosInstance.get("/item-list", {
-      headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: `Bearer ${token}`,
-      },
-    }).then((res) => {
-      if (res.data.status == 200) {
-        setHeaders(res.data.header);
-        setColumns(res.data.data);
-      }
-      if (res.data.status == 401) {
-        toast.error(res.data.message, {
-          position: "top-right",
-          style: {
-            background: "white",
-            color: "black",
-          },
-        });
-        localStorage.removeItem("refreshToken");
-        router.push("/");
-      }
-    });
+    axiosInstance
+      .get("/item-list", {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        if (res.data.status == 200) {
+          setHeaders(res.data.header);
+          setColumns(res.data.data);
+        }
+        if (res.data.status == 401) {
+          toast.error(res.data.message, {
+            position: "top-right",
+            style: {
+              background: "white",
+              color: "black",
+            },
+          });
+          localStorage.removeItem("refreshToken");
+          router.push("/");
+        }
+      });
   }, []);
 
   const totalPages = Math.ceil(columns.length / itemsPerPage);
@@ -61,13 +63,13 @@ const ViewAllItemsData = () => {
   const copyToClipboard = (e, copy_url) => {
     e.stopPropagation();
     try {
-      const currentURL = 'https://esgledger.co/' + copy_url;
+      const currentURL = "https://esgledger.co/" + copy_url;
 
       if (navigator.clipboard) {
         navigator.clipboard.writeText(currentURL);
         setCopySuccess("URL copied to clipboard!");
       } else {
-        const textArea = document.createElement('textarea');
+        const textArea = document.createElement("textarea");
         textArea.value = currentURL;
         document.body.appendChild(textArea);
         textArea.select();
@@ -100,27 +102,26 @@ const ViewAllItemsData = () => {
     window.location.href = `/${lsgUniqueId}`;
   };
 
-
-
   return (
-    <div className='container-fluid'>
+    <div className="container-fluid">
       <div>
-        <div className='data-search-input-div'>
+        <div className="data-search-input-div">
           <span>
             <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='16'
-              height='16'
-              fill='currentColor'
-              class='bi bi-search'
-              viewBox='0 0 16 16'>
-              <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-search"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
             </svg>
           </span>
           <input
-            className='data-search-input'
-            type='search'
-            placeholder='Search by “brand name, Item, Product”'
+            className="data-search-input"
+            type="search"
+            placeholder="Search by “brand name, Item, Product”"
           />
         </div>
 
@@ -155,23 +156,23 @@ const ViewAllItemsData = () => {
         </div> */}
 
         <div>
-          <div className='mt-4'>
-            <table className='full-table'>
+          <div className="mt-4">
+            <table className="full-table">
               <thead>
                 <tr>
                   {headers.length > 0 &&
                     headers.map((item) => (
-                      <th className='table-nav' scope='col'>
-                        <span className='table-th'> {item}</span>
+                      <th className="table-nav" scope="col">
+                        <span className="table-th"> {item}</span>
                       </th>
                     ))}
 
-                  <th className='table-nav' scope='col'>
-                    <span className='table-th'> QR code</span>
+                  <th className="table-nav" scope="col">
+                    <span className="table-th"> QR code</span>
                   </th>
 
-                  <th className='table-nav' scope='col'>
-                    <span className='table-th'> Action</span>
+                  <th className="table-nav" scope="col">
+                    <span className="table-th"> Action</span>
                   </th>
                   {/* <th className='table-navs' scope='col'>
                     <p className='table-ths'> Action</p>
@@ -185,30 +186,47 @@ const ViewAllItemsData = () => {
                 </tr>
               </thead>
               <tbody>
-                {columns.length > 0 ?
+                {columns.length > 0 ? (
                   columns.map((item, index) => (
                     <>
                       <tr
                         key={index}
-                        className='data-tr'
-                        onClick={() => handleRowClick(item["slug"])}>
+                        className="data-tr"
+                        onClick={() => handleRowClick(item["slug"])}
+                      >
                         {headers.length > 0 &&
                           headers.map((head) => (
-                            <td className='data-td'>
-                              <span className='data-th-text'>{item[head] != null ? item[head] :"N/A"}</span>
+                            <td className="data-td">
+                              <span className="data-th-text">
+                                {item[head] != null ? item[head] : "N/A"}
+                              </span>
                             </td>
                           ))}
 
                         <td>
-                          <div className='tabl-icon '>
+                          <div className="tabl-icon ">
                             {/* {item.img1} {item.img1} style={{ height: '20px', width: '15px', marginTop: '-10px' }}*/}
-                            <QRCodeComponent value={'https://esgledger.co/' + item['slug']} size={50} slug={item['slug']} />
+                            <QRCodeComponent
+                              value={"https://esgledger.co/" + item["slug"]}
+                              size={50}
+                              slug={item["slug"]}
+                            />
                           </div>
                         </td>
                         <td>
-
-                          <svg onClick={(e) => copyToClipboard(e, item['slug'])} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-copy" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z" />
+                          <svg
+                            onClick={(e) => copyToClipboard(e, item["slug"])}
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-copy"
+                            viewBox="0 0 16 16"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"
+                            />
                           </svg>
                         </td>
                         {/* <td>
@@ -231,31 +249,37 @@ const ViewAllItemsData = () => {
                       </td> */}
                       </tr>
                       <tr>
-                        <td colSpan={headers.length + 2} className="text-center"></td>
+                        <td
+                          colSpan={headers.length + 2}
+                          className="text-center"
+                        ></td>
                       </tr>
                     </>
                   ))
-                  :
+                ) : (
                   <>
                     <tr>
-                      <td colSpan={headers.length + 2} className="text-center">No data found</td>
+                      <td colSpan={headers.length + 2} className="text-center">
+                        No data found
+                      </td>
                     </tr>
                   </>
-                }
+                )}
               </tbody>
             </table>
 
-            <div className='pagination'>
+            <div className="pagination">
               <svg
                 onClick={() => handlePageChange(currentPage - 1)}
                 disabled={currentPage === 1}
-                xmlns='http://www.w3.org/2000/svg'
-                width='20'
-                height='20'
-                fill='#86C6CA'
-                class='bi bi-caret-left-fill'
-                viewBox='0 0 16 16'>
-                <path d='m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z' />
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="#86C6CA"
+                class="bi bi-caret-left-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
               </svg>
 
               <span>{` ${currentPage} / ${totalPages}`}</span>
@@ -263,13 +287,14 @@ const ViewAllItemsData = () => {
               <svg
                 onClick={() => handlePageChange(currentPage + 1)}
                 disabled={currentPage === totalPages}
-                xmlns='http://www.w3.org/2000/svg'
-                width='16'
-                height='16'
-                fill='#86C6CA'
-                class='bi bi-caret-right-fill'
-                viewBox='0 0 16 16'>
-                <path d='m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z' />
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="#86C6CA"
+                class="bi bi-caret-right-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
               </svg>
             </div>
           </div>
