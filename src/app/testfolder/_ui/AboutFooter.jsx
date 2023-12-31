640bf05af14d555543ddca88657075b77bc9181c
morshedulmunna <morshedulmunna1@gmail.com>
2023-12-31 10:29:53 +0600

    About Page with Footer Full Responsive

--- src/app/testfolder/_ui/AboutFooter.jsx
+++ src/app/testfolder/_ui/AboutFooter.jsx
@@ -3,13 +3,20 @@ import logo from "../_assets/Footer-logo.svg";
 import social from "../_assets/social.svg";
 import Image from "next/image";
 import "../_assets/about-footer.css";
+import Link from "next/link";
 
 export default function AboutFooter() {
   return (
     <div className="footer__wrapper">
-      <dir className="footer__content__wrapper">
+      <div className="footer__content__wrapper">
         <div className="footer__content">
           <div className="logo__social">
+            <div className="special__mobile">
+              <p>
+                For inquiries or more information please contact:
+                enquiries@esgledger.co
+              </p>
+            </div>
             <div className="logo">
               <Image src={logo} alt="logo" />
               <p>
@@ -23,9 +30,20 @@ export default function AboutFooter() {
             </div>
           </div>
 
-          <div className="footer__bottom"></div>
+          <div className="footer__bottom">
+            <p>© 2023 OneChain Ltd.</p>
+
+            <div className="link__area">
+              <Link className="link" href={"#"}>
+                Privacy Policy
+              </Link>
+              <Link className="link" href={"#"}>
+                Terms of Service
+              </Link>
+            </div>
+          </div>
         </div>
-      </dir>
+      </div>
     </div>
   );
 }
