99eee0771fbb7ed96ddf8a2db99b088f4b1d1cda
morshedulmunna <morshedulmunna1@gmail.com>
2023-12-31 14:34:27 +0600

    social icon added

--- src/app/testfolder/_ui/AboutFooter.jsx
+++ src/app/testfolder/_ui/AboutFooter.jsx
@@ -3,6 +3,7 @@ import logo from "../_assets/Footer-logo.svg";
 import social from "../_assets/social.svg";
 import Image from "next/image";
 import "../_assets/about-footer.css";
+import {facebook, instagram, linkedin, youtube} from "../_assets";
 import Link from "next/link";
 
 export default function AboutFooter() {
@@ -25,7 +26,38 @@ export default function AboutFooter() {
               </p>
             </div>
             <div className="social">
-              <Image src={social} alt="Social" />
+              <div className="social__ac__link">
+                {[
+                  {
+                    href: "#",
+                    icon: facebook,
+                    alt: "facebook",
+                  },
+                  {
+                    href: "#",
+                    icon: instagram,
+                    alt: "instagram",
+                  },
+                  {
+                    href: "#",
+                    icon: linkedin,
+                    alt: "linkedin",
+                  },
+                  {
+                    href: "#",
+                    icon: youtube,
+                    alt: "youtube",
+                  },
+                ].map((each, index) => (
+                  <Link
+                    className="social_icon_link"
+                    key={index}
+                    href={each.href}
+                  >
+                    <Image src={each.icon} alt={each.alt} />
+                  </Link>
+                ))}
+              </div>
               <p>Join the Community</p>
             </div>
           </div>
