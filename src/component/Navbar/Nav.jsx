80411c7fa912f9891c2cc25f0e8790dba125d27c
morshedulmunna <morshedulmunna1@gmail.com>
2024-01-01 13:46:08 +0600

    mobile-nav bar issue fix

--- src/component/Navbar/Nav.jsx
+++ src/component/Navbar/Nav.jsx
@@ -1,12 +1,13 @@
 "use client";
 import Image from "next/image";
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import PageLogo from "../../../public/pageLogonavas.png";
 import PageLogomob from "../../../public/pageLogomd.png";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
 import companyLogo from "../../../public/pageLogonavas.png";
 import companyLogo1 from "../../../public/pageLogomd.png";
+import "./mobileNav.css";
 
 const Nav = () => {
   const [navbarColor, setNavbarColor] = useState(null);
@@ -85,73 +86,77 @@ const Nav = () => {
 
   return (
     <div>
-      <div className='full-navbar-div'>
-        <div className='row web-nav'>
-          <div className='col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2'></div>
-          <div className='col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4'>
-            <div className='nav-logo-img-div'>
-              <Link href='/'>
-                <img className='nav-logo-img' src={PageLogo.src} alt='' />
+      <div className="full-navbar-div">
+        <div className="row web-nav">
+          <div className="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
+          <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+            <div className="nav-logo-img-div">
+              <Link href="/">
+                <img className="nav-logo-img" src={PageLogo.src} alt="" />
               </Link>
             </div>
           </div>
 
-          <div className='col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 '>
+          <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 ">
             <div>
-              <div className='nav-items-div'>
-                <Link href='/admin'>Home</Link>
-                <Link className='contact-nav-a' href='/brands/login'>
+              <div className="nav-items-div">
+                <Link href="/admin">Home</Link>
+                <Link className="contact-nav-a" href="/brands/login">
                   {" "}
                   <p>Brands</p>{" "}
                   <span>
                     <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      width='10'
-                      height='10'
-                      fill='currentColor'
-                      class='bi bi-chevron-down'
-                      viewBox='0 0 16 16'>
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="10"
+                      height="10"
+                      fill="currentColor"
+                      class="bi bi-chevron-down"
+                      viewBox="0 0 16 16"
+                    >
                       <path
-                        fill-rule='evenodd'
-                        d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'
+                        fill-rule="evenodd"
+                        d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
                       />
                     </svg>
                   </span>
                 </Link>
-                <Link href='/about'>About ESGLedger</Link>
+                <Link href="/about">About ESGLedger</Link>
                 <Link
-                  className='contact-nav-a'
-                  href='https://www.onechainscan.io/'
-                  target='_blank'>
+                  className="contact-nav-a"
+                  href="https://www.onechainscan.io/"
+                  target="_blank"
+                >
                   <p>Explorer</p>{" "}
                   <span>
                     <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      width='10'
-                      height='10'
-                      fill='currentColor'
-                      class='bi bi-chevron-down'
-                      viewBox='0 0 16 16'>
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="10"
+                      height="10"
+                      fill="currentColor"
+                      class="bi bi-chevron-down"
+                      viewBox="0 0 16 16"
+                    >
                       <path
-                        fill-rule='evenodd'
-                        d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'
+                        fill-rule="evenodd"
+                        d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
                       />
                     </svg>
                   </span>
                 </Link>
-                <Link className='contact-nav-a' href='/contactUs'>
+                <Link className="contact-nav-a" href="/contactUs">
                   <p>Contact</p>{" "}
                   <span>
                     <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      width='10'
-                      height='10'
-                      fill='currentColor'
-                      class='bi bi-chevron-down'
-                      viewBox='0 0 16 16'>
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="10"
+                      height="10"
+                      fill="currentColor"
+                      class="bi bi-chevron-down"
+                      viewBox="0 0 16 16"
+                    >
                       <path
-                        fill-rule='evenodd'
-                        d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'
+                        fill-rule="evenodd"
+                        d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
                       />
                     </svg>
                   </span>
@@ -159,113 +164,162 @@ const Nav = () => {
               </div>
             </div>
           </div>
-          <div className='col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2'></div>
+          <div className="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
         </div>
       </div>
-      <div className='mobile-nav-div'>
+      <div className="mobile-nav-div">
         <nav
-          class='navbar navbar-expand-lg mob-nav fixed-top'
-          style={{ backgroundColor: navbarColor }}>
-          <div class=''>
-            <div className='nav-mob-logo-img-div'>
-              <Link href='/landingPage'>
-                <img className='nav-mob-logo-img' src={logoUrl.src} alt='' />
+          class="navbar navbar-expand-lg mob-nav fixed-top w-100"
+          style={{backgroundColor: navbarColor}}
+        >
+          <div className="w-100">
+            <div className="w-100  d-flex align-items-center justify-content-between">
+              <Link
+                style={{
+                  marginLeft: "50px",
+                }}
+                href="/landingPage"
+              >
+                <img className="nav-mob-logo-img" src={logoUrl.src} alt="" />
               </Link>
               <button
-                style={{ backgroundColor: dropdownColor }}
-                className='navbar-toggler'
-                type='button'
-                data-bs-toggle='collapse'
-                data-bs-target='#navbarTogglerDemo01'
-                aria-controls='navbarTogglerDemo01'
-                aria-expanded='false'
-                aria-label='Toggle navigation'>
-                <span className='svg-iconic'>
+                style={{
+                  backgroundColor: "white",
+                  border: "none",
+                }}
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarTogglerDemo01"
+                aria-controls="navbarTogglerDemo01"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="svg-iconic">
                   <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='18'
-                    height='18'
-                    viewBox='0 0 18 18'
-                    fill='none'>
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 18 18"
+                    fill="none"
+                  >
                     <path
-                      d='M1 1H17.4706'
-                      stroke='#066F69'
-                      stroke-linecap='round'
+                      d="M1 1H17.4706"
+                      stroke="#066F69"
+                      stroke-linecap="round"
                     />
                     <path
-                      d='M1 9H17.4706'
-                      stroke='#066F69'
-                      stroke-linecap='round'
+                      d="M1 9H17.4706"
+                      stroke="#066F69"
+                      stroke-linecap="round"
                     />
                     <path
-                      d='M1 17H17.4706'
-                      stroke='#066F69'
-                      stroke-linecap='round'
+                      d="M1 17H17.4706"
+                      stroke="#066F69"
+                      stroke-linecap="round"
                     />
                   </svg>
                 </span>
               </button>
             </div>
-            <div className='collapse navbar-collapse' id='navbarTogglerDemo01'>
-              <div className='nav-items-div-mob'>
-                <Link className='contact-nav-a-mov' href='/admin'>
-                  <p> Home</p>
-                </Link>
-                <Link className='contact-nav-a-mov' href='/brands/login'>
-                  {" "}
-                  <p>Brands</p>{" "}
-                  <span>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      width='10'
-                      height='10'
-                      fill='currentColor'
-                      class='bi bi-chevron-down'
-                      viewBox='0 0 16 16'>
-                      <path
-                        fill-rule='evenodd'
-                        d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'
-                      />
-                    </svg>
-                  </span>
-                </Link>
-                <Link className='contact-nav-a-mov' href='/about'>
-                  <p> About ESGLedger</p>
-                </Link>
-                <Link className='contact-nav-a-mov' href='#'>
-                  <p>Explorer</p>{" "}
-                  <span>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      width='10'
-                      height='10'
-                      fill='currentColor'
-                      class='bi bi-chevron-down'
-                      viewBox='0 0 16 16'>
-                      <path
-                        fill-rule='evenodd'
-                        d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'
-                      />
-                    </svg>
-                  </span>
-                </Link>
-                <Link className='contact-nav-a-mov' href='/contactUs'>
-                  <p>Contact</p>{" "}
-                  <span>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      width='10'
-                      height='10'
-                      fill='currentColor'
-                      class='bi bi-chevron-down'
-                      viewBox='0 0 16 16'>
-                      <path
-                        fill-rule='evenodd'
-                        d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'
-                      />
-                    </svg>
-                  </span>
-                </Link>
+            <div
+              style={{
+                backgroundColor: navbarColor,
+              }}
+              className="collapse navbar-collapse "
+              id="navbarTogglerDemo01"
+            >
+              <div
+                style={{
+                  paddingTop: "10px",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100%",
+                    textAlign: "center",
+                  }}
+                  className="nav-items-div-mob text-center"
+                >
+                  <Link className="contact-nav-a-mov" href="/admin">
+                    <p> Home</p>
+                  </Link>
+                  <Link className="contact-nav-a-mov" href="/brands/login">
+                    {" "}
+                    <p>Brands</p>{" "}
+                    {/* <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="10"
+                        height="10"
+                        fill="currentColor"
+                        class="bi bi-chevron-down"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                        />
+                      </svg>
+                    </span> */}
+                  </Link>
+                  <Link className="contact-nav-a-mov" href="/about">
+                    <p> About ESGLedger</p>
+                  </Link>
+                  <Link className="contact-nav-a-mov" href="#">
+                    <p>Explorer</p>{" "}
+                    {/* <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="10"
+                        height="10"
+                        fill="currentColor"
+                        class="bi bi-chevron-down"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                        />
+                      </svg>
+                    </span> */}
+                  </Link>
+                  <Link className="contact-nav-a-mov" href="/contactUs">
+                    <p>Contact</p>{" "}
+                    {/* <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="10"
+                        height="10"
+                        fill="currentColor"
+                        class="bi bi-chevron-down"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                        />
+                      </svg>
+                    </span> */}
+                  </Link>
+
+                  <div
+                    style={{
+                      height: "5px",
+                      width: "100%",
+                      backgroundColor: "#066F69",
+                      borderRadius: "5px",
+                    }}
+                  ></div>
+                </div>
               </div>
             </div>
           </div>
