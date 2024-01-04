87ba40e424390201ee382ba954d510cfa9daaa3c
Md Anik Islam <anikislam668@gmail.com>
2024-01-04 18:54:51 +0600

    cc

--- src/component/DashboardAll/IssuDetails.jsx
+++ src/component/DashboardAll/IssuDetails.jsx
@@ -7,6 +7,49 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import * as XLSX from 'xlsx';
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+import Modal from "@mui/material/Modal";
+
+import ModalFile from "../../../public/modalfile.svg";
+import ModalConferm from "../../../public/successfull-modal.svg";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "90%",
+  bgcolor: "background.paper",
+  border: "none",
+  boxShadow: 24,
+  p: 4,
+};
+const styletwo = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "90%",
+  bgcolor: "background.paper",
+  border: "none",
+  boxShadow: 24,
+  p: 4,
+};
+const stylethree = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "90%",
+  bgcolor: "background.paper",
+  border: "none",
+  boxShadow: 24,
+  p: 4,
+};
 
 const IssuDetails = () => {
   const itemsPerPage = 15;
@@ -14,6 +57,7 @@ const IssuDetails = () => {
 
   const [headers, setHeaders] = useState([]);
   const [columns, setColumns] = useState([]);
+  const [rows, setRows] = useState([]);
   const totalPages = Math.ceil(columns.length / itemsPerPage);
 
   const startIndex = (currentPage - 1) * itemsPerPage;
@@ -21,6 +65,18 @@ const IssuDetails = () => {
   const currentData = columns.slice(startIndex, endIndex);
   const [isGenerate, SetIsGenerate] = useState(0);
   const [selectedFileName, setSelectedFileName] = useState("Doc name");
+  const [tableContent, setTableContent] = useState(null);
+
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [opent, setOpent] = React.useState(false);
+  const handleOpent = () => setOpent(true);
+  const handleCloset = () => setOpent(false);
+  const [openthree, setOpenthree] = React.useState(false);
+  const handleOpenthree = () => setOpenthree(true);
+  const handleClosethree = () => setOpenthree(false);
 
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
@@ -87,7 +143,7 @@ const IssuDetails = () => {
     }
   };
 
-  const handleFileChange = (e) => {
+  const handleFileChange = async (e) => {
     SetIsGenerate(1);
     // console.log("handlefile change");
     const selectedFile = e.target.files[0];
@@ -101,6 +157,39 @@ const IssuDetails = () => {
       token = localStorage.getItem("refreshToken");
     }
 
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const data = e.target.result;
+
+      // Check if the data is not empty before trying to read
+      if (data) {
+        const workbook = XLSX.read(data, { type: 'binary' });
+
+        // Ensure workbook is defined before accessing its properties
+        if (workbook && workbook.SheetNames) {
+          const sheetName = workbook.SheetNames[0];
+          const sheet = workbook.Sheets[sheetName];
+
+          // Extract header
+          const headers = XLSX.utils.sheet_to_json(sheet, { header: 1 })[0];
+          setHeaders(headers);
+
+          // Extract columns
+          const columnData = XLSX.utils.sheet_to_json(sheet, { header: 1, range: 2 });
+          setColumns(columnData);
+        } else {
+          console.error('Invalid Excel file');
+        }
+      } else {
+        console.error('Empty file content');
+      }
+    };
+
+    reader.readAsBinaryString(selectedFile);
+
+
+    SetIsGenerate(2);
+    return;
     axiosInstance
       .post(`/upload-csv`, formData, {
         headers: {
@@ -115,7 +204,6 @@ const IssuDetails = () => {
           SetIsGenerate(2);
         }
         if (res.data.status == 201) {
-          // toast.error(res.data.message)
           toast.error(res.data.message, {
             position: "top-right",
             style: {
@@ -137,47 +225,68 @@ const IssuDetails = () => {
         }
       });
   };
+
+  console.log("headers", headers);
+  console.log("columns", columns);
   const handleDelete = (deleteId) => {
+
+    const updatedColumns = [...columns];
+
+    // Remove the row at the specified index
+    updatedColumns.splice(deleteId, 1);
+
+    // Update state with the new columns array
+    setColumns(updatedColumns);
+
+    toast.success("Item Removed", {
+      position: "top-right",
+      style: {
+        background: "white",
+        color: "black",
+      },
+    });
+
+
+  };
+
+
+  const saveMint = (e) => {
     const formData = {
-      id: deleteId,
-    };
+      header: headers,
+      column: columns,
+    }
+
     let token = "";
 
     if (typeof window !== "undefined") {
       token = localStorage.getItem("refreshToken");
     }
 
-    axiosInstance
-      .post("/delete-item", formData, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((res) => {
-        if (res.data.status == 200) {
-          const indexToDelete = columns.findIndex(
-            (item) => item.id === deleteId
-          );
+    axiosInstance.post('/save-mint-data', formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((res) => {
+      if (res.data.status == 200) {
+        handleOpenthree(true)
+      }
+      if (res.data.status == 401) {
+        toast.error(res.data.message, {
+          position: "top-right",
+          style: {
+            background: "white",
+            color: "black",
+          },
+        });
+        localStorage.removeItem("refreshToken");
+        router.push("/");
 
-          if (indexToDelete !== -1) {
-            // Create a new array without the deleted item
-            const updatedColumns = [...columns];
-            updatedColumns.splice(indexToDelete, 1);
+      }
+    })
+  }
 
-            // Update the state with the new array
-            setColumns(updatedColumns);
 
-            toast.success("Item Removed", {
-              position: "top-right",
-              style: {
-                background: "white",
-                color: "black",
-              },
-            });
-          }
-        }
-      });
-  };
   return (
     <div className='container-fluid'>
       <div className='issuess-upload-full-div'>
@@ -214,18 +323,17 @@ const IssuDetails = () => {
           {isGenerate == 2 && (
             <div className='previe-issue-text'>
               <p className='previe-issue-btn-text'>Clear</p>
-              <Link className='previe-issue-btn-link' href='/allitems'>
+              <span className='previe-issue-btn-link' onClick={handleOpen}>
                 <p className='previe-issue-btn'> Mint</p>
-              </Link>
+              </span>
             </div>
           )}
         </div>
 
         <div>
           <div
-            className={` issue-data-table-div ${
-              isGenerate != 2 ? "issue-box-details" : ""
-            }`}>
+            className={` issue-data-table-div ${isGenerate != 2 ? "issue-box-details" : ""
+              }`}>
             {isGenerate == 0 && (
               <div className='no-file-select-div'>
                 <p>No File Select</p>
@@ -239,16 +347,24 @@ const IssuDetails = () => {
                 </p>
               </div>
             )}
+            <div dangerouslySetInnerHTML={{ __html: tableContent }} />
+
             {isGenerate == 2 && (
               <table className='full-table'>
                 <thead>
+
+
                   <tr>
-                    {headers.length > 0 &&
-                      headers.map((item) => (
-                        <th className='table-nav' scope='col'>
-                          <span className='table-th'> {item}</span>
-                        </th>
-                      ))}
+                    {headers?.map((header, i) => (
+                      <th className='table-nav' scope='col' key={i}>
+
+                        <span className='table-th'>
+                          {header}
+                        </span>
+
+                      </th>
+                    ))}
+
 
                     <th className='table-navs' scope='col'>
                       <span className='table-ths'> QR code</span>
@@ -260,31 +376,23 @@ const IssuDetails = () => {
                       <span className='table-ths'> QR code</span>
                     </th>
                   </tr>
-                  <tr>
-                    <th
-                      colSpan={headers.length + 2}
-                      className='text-center'></th>
-                  </tr>
                 </thead>
                 <tbody>
                   {columns.map((item, index) => (
                     <>
-                      <tr key={index} className='data-tr'>
-                        {headers.length > 0 &&
-                          headers.map((head) => (
-                            <td className='data-td'>
-                              {/* <p className='data-th-text'>{item.brand}</p> */}
-                              <span className='data-th-text'>
-                                {item[head] != null ? item[head] : "N/A"}
-                              </span>
-                            </td>
-                          ))}
-
+                      <tr key={index}>
+                        {item?.map((data, i) => {
+                          return <td className='data-td' key={i}>
+                            <span className='data-th-text'>
+                              {data}
+                            </span>
+                          </td>;
+                        })}
                         <td className='data-td'>
                           <span
                             className='data-td cursor-pointer text-red'
                             onClick={() => {
-                              handleDelete(item.id);
+                              handleDelete(index);
                             }}>
                             Delete
                           </span>
@@ -351,7 +459,91 @@ const IssuDetails = () => {
           </div>
         </div>
       </div>
+      <Modal
+        open={opent}
+        onClose={handleCloset}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'>
+        <Box sx={styletwo}>
+          <div>
+            <div className='mod-f-img-div'>
+              <div className='spinner-border' role='status'>
+                <span className='visually-hidden'>Loading...</span>
+              </div>
+
+              <p className='mod-f-buffering-text'>
+                Your data is being recorded on the blockchain. Kindly wait a
+                moment.
+              </p>
+            </div>
+            {/* <div className='button-div'>
+                  <button className='return-m-btn'>RETURN TO EDIT</button>
+                  <button className='conferm-m-btn'>CONFIRM TO PROCEED</button>
+                </div> */}
+          </div>
+        </Box>
+      </Modal>
+
+      <Modal
+        open={openthree}
+        onClose={handleClosethree}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'>
+        <Box sx={stylethree}>
+          <div>
+            <div className='mod-f-img-div'>
+              <img className='mod-f-img' src={ModalConferm.src} alt='' />
+
+              <p className='mod-f-succes-text'>
+                Your data is being recorded on the blockchain. Kindly wait a
+                moment.
+              </p>
+            </div>
+            <div className='button-div'>
+
+              <Link className='conferm-m-btn-link' href='/allitems'>
+                VIEW MY PRODUCTS
+              </Link>
+            </div>
+          </div>
+        </Box>
+      </Modal>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'>
+        <Box sx={style}>
+          <div>
+            <div className='mod-f-img-div'>
+              <img className='mod-f-img' src={ModalFile.src} alt='' />
+              <p className='mod-f-uplod-text'>
+                By clicking 'Upload & Mint,'
+              </p>
+              <p className='mod-f-uplod-textp'>
+                your data will be permanently stored on the blockchain, and
+                this action cannot be reverted.
+              </p>
+              <p className='mod-f-uplod-textp2'>
+                Please verify all information for accuracy before
+                proceeding.
+              </p>
+            </div>
+            <div className='button-div'>
+              <button className='return-m-btn' onClick={handleClose}>RETURN TO EDIT</button>
+              <button className='conferm-m-btn' onClick={() => {
+                saveMint();
+                handleOpent(true)
+                handleClose()
+              }}>CONFIRM TO PROCEED</button>
+            </div>
+          </div>
+        </Box>
+      </Modal>
+
     </div>
+
+
   );
 };
 
